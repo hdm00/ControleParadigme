8636a87c5db304fdbf69a7a8016ddace2726aa5e
Hugo <hugodemarco@ymail.com>
2024-05-15 16:40:54 +0200

    Ajout de la classe main, création d'une ArrayListe qui contient objets membres et employes. OK.

---
+++ Java/TPJava/src/tpjava/Principal.java
@@ -0,0 +1,22 @@
+package tpjava;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Principal {
+	
+//pas de majuscule au main car l'IDE eclipse ne le reconnait pas comme programme principal autrement
+	public static void main(String[] args) {
+
+		Date date = new Date(); //Actuellement
+		
+		ArrayList<Personne> liste = new ArrayList<Personne>();
+		Membre membre1 = new Membre("de Marco","Hugo", 1, date, "Actif");
+		Employe employe1 = new Employe("Cloux", "Erwan", 2, "Libraire", 1700.0);
+		liste.add(membre1);
+		liste.add(employe1);
+		
+		System.out.println(liste);
+	}
+
+}
