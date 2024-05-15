3d8770c6b06a91106e5cc02e0c24bed53de62882
Hugo <hugodemarco@ymail.com>
2024-05-15 12:08:23 +0200

    Exercice 1 ok.

--- Exercice1/bus.js
+++ Exercice1/bus.js
@@ -2,5 +2,14 @@
 number([[10,0],[3,5],[5,8]]) -> 5
 number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]) -> 17
 number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]) -> 21
+
+A effectuer : Créer une fonction en programmation fonctionnelle javascript, prend argument le tableau de tableaux, et dans chaque pair de valeur, additionne le nombre de gauche et soustrait le nombre de droite.
 */
 
+let tableau = [[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]];
+
+const comptePersonnes = tableau.reduce((acc, [gauche, droite]) => {
+    return acc + gauche - droite;
+}, 0);
+
+console.log(comptePersonnes);
